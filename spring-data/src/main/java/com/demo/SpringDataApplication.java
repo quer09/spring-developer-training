a30db419f1b65be71259e02f55de041d77e69cae
quervin <quervin.hidalgo@grupopromerica.com>
2023-03-16 19:11:58 -0600

    Creación de la conexión a la base de datos

--- spring-data/src/main/java/com/demo/SpringDataApplication.java
+++ spring-data/src/main/java/com/demo/SpringDataApplication.java
@@ -1,7 +1,12 @@
 package com.demo;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
+
 
 @SpringBootApplication
 public class SpringDataApplication {
@@ -9,5 +14,12 @@ public class SpringDataApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseServer() throws SQLException {
+		return Server.createTcpServer(
+				"-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+	}
+
 
 }
