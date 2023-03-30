d2e71abbe36645ba522b5dc8b9381ba0bb673958
quervin <quervin.hidalgo@grupopromerica.com>
2023-03-29 19:39:49 -0600

    Security Config

--- spring-data/src/main/java/com/demo/springjms/pubsub/ProcesadorNotificacionClientes.java
+++ spring-data/src/main/java/com/demo/springjms/pubsub/ProcesadorNotificacionClientes.java
@@ -1,6 +1,8 @@
 package com.demo.springjms.pubsub;
 
 import com.demo.springdata.dto.CuentaDto;
+import com.twilio.Twilio;
+import com.twilio.type.PhoneNumber;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.integration.annotation.ServiceActivator;
@@ -12,10 +14,21 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @AllArgsConstructor
 public class ProcesadorNotificacionClientes {
+
+    private final static String TWILIO_ACCOUNT_SID = "";
+    private final static String TWILIO_AUTH_TOKEN = "";
+    private final static String TWILIO_MESSAGE_SID = "";
+
     @ServiceActivator(inputChannel = "pubSubNotification")
     public Message<String> consumirMensajeParaEnvioFormatoClientes(Message<CuentaDto> message){
         CuentaDto cuentaDto = message.getPayload();
         log.info("consumirMensajeParaEnvioFormatoClientes");
+        // Puede tener la logica que necesitemos
+        String sms = "Hola desde Twilio SMS";
+        Twilio.init(TWILIO_ACCOUNT_SID.trim(), TWILIO_AUTH_TOKEN.trim());
+        com.twilio.rest.api.v2010.account.Message.creator(new PhoneNumber("+593999714503"),
+                TWILIO_MESSAGE_SID.trim(), sms).create();
+
         // Puede tener la logica que necesitemos
         // Envio SMS con formato a Cliente
         return MessageBuilder.withPayload("Mensaje recibido pór ProcesadorNotificacionClientes")
